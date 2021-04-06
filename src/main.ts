97258392bdb5dd8b0aa0f7de42b141dff4a99786
puntart <thiagopuntar@gmail.com>
2021-04-06 14:45:28 -0300

    setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ingaia Base Price API')
+    .setVersion('1.0')
+    .setDescription('Api1 - Preço do metro quadrado')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
   const port = process.env.PORT || 3000;
   console.log(`Running on port ${port}`);
   app.enableCors();
--- src/square-meter-price/dtos/create-square-meter-price.dto.ts
+++ src/square-meter-price/dtos/create-square-meter-price.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateSquareMeterPriceDto {
+  @ApiProperty({ type: Number, description: 'Float number' })
   value: number;
 }
