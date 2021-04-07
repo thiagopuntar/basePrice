7e7497a76e9937dcb59da7ce1420dfa1bcb0d12b
puntart <thiagopuntar@gmail.com>
2021-04-07 17:24:30 -0300

    implementing tests

--- src/square-meter-price/square-meter-price.service.spec.ts
+++ src/square-meter-price/square-meter-price.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateSquareMeterPriceDto } from './dtos/create-square-meter-price.dto';
+import { GetSquareMeterPriceDto } from './dtos/GetSquareMeterPrice.dto';
+import { SquareMeterPriceRepository } from './square-meter-price.repository';
 import { SquareMeterPriceService } from './square-meter-price.service';
 
 describe('SquareMeterPriceService', () => {
@@ -6,13 +9,34 @@ describe('SquareMeterPriceService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SquareMeterPriceService],
+      providers: [SquareMeterPriceService, SquareMeterPriceRepository],
+      imports: [SquareMeterPriceRepository],
     }).compile();
 
     service = module.get<SquareMeterPriceService>(SquareMeterPriceService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createSquareMeterPrice', () => {
+    it('should return price id', async () => {
+      SquareMeterPriceRepository.prototype.insert = jest
+        .fn()
+        .mockResolvedValue({ raw: { id: 1 } });
+
+      const data: CreateSquareMeterPriceDto = { value: 1 };
+      const result = await service.createSquareMeterPrice(data);
+      expect(result).toHaveProperty('id');
+    });
+  });
+
+  describe('getSquareMeterPrice', () => {
+    it('should return SquareMeterPrice', async () => {
+      SquareMeterPriceRepository.prototype.findOne = jest
+        .fn()
+        .mockResolvedValue({ value: 1 });
+
+      const filter: GetSquareMeterPriceDto = { zipCode: '' };
+      const result = await service.getSquareMeterPrice(filter);
+      expect(result).toHaveProperty('value', 1);
+    });
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { SquareMeterPriceModule } from '../src/square-meter-price/square-meter-price.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [SquareMeterPriceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/GET square-meter-price', () => {
+    return request(app.getHttpServer()).get('/square-meter-price').expect(200);
+  });
+
+  it('/POST square-meter-price', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/square-meter-price')
+      .send({ value: 1 })
+      .expect(201);
   });
 });
